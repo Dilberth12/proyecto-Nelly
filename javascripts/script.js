22bb203469426ae78934bf25263ebd3cd07b8534
Dilberth12 <dilberthvalqui@gmail.com>
2023-10-02 19:16:13 -0500

    avance

---
+++ javascripts/script.js
@@ -0,0 +1,19 @@
+const btn_ocultar = document.querySelector('.ocultar');
+
+btn_ocultar.addEventListener('click', function (){
+    document.getElementById('menu').classList.toggle('oculto');
+})
+
+//ANIMACION DEL ICOMNO DE MENU
+document.querySelector(".bars__menu").addEventListener("click", animateBars);
+
+var line1__bars = document.querySelector(".line1__bars-menu");
+var line2__bars = document.querySelector(".line2__bars-menu");
+var line3__bars = document.querySelector(".line3__bars-menu");
+
+function animateBars(){
+    line1__bars.classList.toggle("activeline1__bars-menu");
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    line3__bars.classList.toggle("activeline3__bars-menu");
+}
+
